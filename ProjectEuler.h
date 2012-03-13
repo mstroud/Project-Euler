c5016060aea2ecbd0fb8e3d40001f3ad8ef28c26
Matt Stroud <mstroud6@tx.rr.com>
2012-03-13 16:58:17 -0500

    Added Makefile, fixed pedantic stuff, consolidated includes

---
+++ ProjectEuler.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int PE1main(void);
+int PE2main(void);
--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "ProjectEuler.h"
 
 int main(int argc, char *argv[])
 {
-    //Usage will ProjectEuler N, where N is the problem number to run
-    //Just for now I'm going to do this as a sanity check...
+    /* Usage will ProjectEuler N, where N is the problem number to run */
+    /* Just for now I'm going to do this as a sanity check...          */
     PE1main();
     PE2main();
     return 0;
