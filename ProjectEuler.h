0644ceaee6a4783329982881555cafc2ae579e0d
mstroud <matt.c.stroud@gmail.com>
2013-03-28 13:48:06 -0500

    Merged updates from upstream master, preserving personal copies of solutions

--- ProjectEuler.h
+++ ProjectEuler.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef __ProjectEulerH
 #define __ProjectEulerH
 
 // PE defines
-#define NUM_PROBLEMS 7
+#define NUM_PROBLEMS 11
 
 // PE data structures
 typedef struct {
@@ -17,9 +18,12 @@ typedef struct {
 int PE1main(pe_data_t*);
 int PE2main(pe_data_t*);
 int PE3main(pe_data_t*);
-//int PE4main(pe_data_t*);
+int PE4main(pe_data_t*);
 int PE5main(pe_data_t*);
-//int PE6main(pe_data_t*);
+int PE6main(pe_data_t*);
 int PE7main(pe_data_t*);
-
+int PE8main(pe_data_t*);
+int PE9main(pe_data_t*);
+int PE10main(pe_data_t*);
+int PE11main(pe_data_t*);
 #endif
